92bd8c1d5fc98dbc792162c0e3f606d2cd0f56f1
k-utsubo <utsuboka@gmail.com>
2017-08-28 11:36:13 +0900

    add

--- sampleAttSeq2Seq.sh
+++ sampleAttSeq2Seq.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-
-#PBS -q G-SINGLE
 #PBS -N attseq2seq
-#PBS -l select=1
-#PBS -j oe
+#PBS -j oe 
+#PBS -l ncpus=1
+#PBS -q SINGLE
 
-. ~/.bashrc
-if [ "$PBS_O_WORKDIR" != "" ];then
-  cd $PBS_O_WORKDIR
-  export PATH=${PBS_O_PATH}
-  export LDPATH=/work/opt/glibc-2.17/lib/ld-linux-x86-64.so.2:$LD_LIBRARY_PATH
-  export LDLIBS=/work/opt/glibc-2.17/lib:/work/opt/gcc485/lib64:/opt/cuda/8.0/lib64:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH
-  export PYTHON_PATH=/work/${USER}/python/py35
+if [ "${PBS_O_WORKDIR}" != "" ];then
+  cd ${PBS_O_WORKDIR}
+  export PYTHON_EGG_CACHE=/work/s1630401/tmp/.python-eggs
+  export OMP_NUM_THREADS=1
 fi
+source ~/.bashrc
 
-python sampleAttSeq2Seq.py --dict ../../data/chat/txt/dict.dat --data ../../data/chat/txt/chat.dat --model att.model --mode train -g 0 --epoch 100 
+aprun python sampleAttSeq2Seq.py --dict ../../data/chat/txt/dict.dat --data ../../data/chat/txt/chat.dat --model att.model --mode train  --epoch 100 
 #aws s3 cp --recursive ../../data/chat/txt  s3://kyodonews.advanced/utsubo/nlp/chat
-for f in *.txt;do
-  aws s3 cp $f  s3://kyodonews.advanced/utsubo/nlp/chat/
-done
-if [ -f att.model ];then
-  echo "sudo shutdown -h now"
-#  sudo shutdown -h now
-fi
+#for f in *.txt;do
+#  aws s3 cp $f  s3://kyodonews.advanced/utsubo/nlp/chat/
+#done
+#if [ -f att.model ];then
+#  echo "sudo shutdown -h now"
+##  sudo shutdown -h now
+#fi
--- sampleSeq2Seq.sh
+++ sampleSeq2Seq.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-
-#PBS -q G-SINGLE
 #PBS -N seq2seq
-#PBS -l select=1
-#PBS -j oe
+#PBS -j oe 
+#PBS -l ncpus=1
+#PBS -q SINGLE
 
-#cd $PBS_O_WORKDIR
+if [ "${PBS_O_WORKDIR}" != "" ];then
+  cd ${PBS_O_WORKDIR}
+  export PYTHON_EGG_CACHE=/work/s1630401/tmp/.python-eggs
+  export OMP_NUM_THREADS=1
+fi
+source ~/.bashrc
 
-. ~/.bashrc
-#export PATH=${PBS_O_PATH}
-
-#export LDPATH=/work/opt/glibc-2.17/lib/ld-linux-x86-64.so.2:$LD_LIBRARY_PATH
-#export LDLIBS=/work/opt/glibc-2.17/lib:/work/opt/gcc485/lib64:/opt/cuda/8.0/lib64:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH
-#export PYTHON_PATH=/work/${USER}/python/py35
-
-python sampleSeq2Seq.py --dict ../../data/chat/txt/dict.dat --data ../../data/chat/txt/chat.dat --mode train --model seq.model  --epoch 100
+aprun python sampleSeq2Seq.py --dict ../../data/chat/txt/dict.dat --data ../../data/chat/txt/chat.dat --mode train --model seq.model  --epoch 100
 #aws s3 cp --recursive ../../data/chat/txt  s3://kyodonews.advanced/utsubo/nlp/chat
 #for f in *.txt;do
 #  aws s3 cp $f  s3://kyodonews.advanced/utsubo/nlp/chat/
