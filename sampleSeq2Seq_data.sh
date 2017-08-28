a96957f1e1bd7324d096915344740ceb3bcd8948
k-utsubo <utsuboka@gmail.com>
2017-08-28 17:55:18 +0900

    bug

--- sampleSeq2Seq_data.sh
+++ sampleSeq2Seq_data.sh
@@ -2,19 +2,17 @@
 
 #PBS -q SINGLE
 #PBS -N seq2seq_data
-#PBS -l select=1
+#PBS -l ncpus=1
 #PBS -j oe
 
-
-. ~/.bashrc
-
-if [ "$PBS_O_WORKER" != "" ];then
-  cd $PBS_O_WORKDIR
-  export PATH=${PBS_O_PATH}
-  export LDPATH=/work/opt/glibc-2.17/lib/ld-linux-x86-64.so.2:$LD_LIBRARY_PATH
-  export LDLIBS=/work/opt/glibc-2.17/lib:/work/opt/gcc485/lib64:/opt/cuda/8.0/lib64:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH
+if [ "${PBS_O_WORKDIR}" != "" ];then
+  cd ${PBS_O_WORKDIR}
+  export PYTHON_EGG_CACHE=/work/s1630401/tmp/.python-eggs
+  export OMP_NUM_THREADS=1
 fi
+source ~/.bashrc
 
 
-python sampleSeq2Seq_data.py --indir ../../data/chat/json/init100 --indict ../../data/chat/txt/dict.dat --outfile ../../data/chat/txt/chat.dat --outdict ../../data/chat/txt/dict.dat
-python sampleSeq2Seq_data.py --indir ../../data/chat/json/rest1046 --indict ../../data/chat/txt/dict.dat --outfile ../../data/chat/txt/chat.dat --outdict ../../data/chat/txt/dict.dat
+
+aprun python ./sampleSeq2Seq_data.py --indir ../../data/chat/json/init100 --indict ../../data/chat/txt/dict.dat --outfile ../../data/chat/txt/chat.dat --outdict ../../data/chat/txt/dict.dat
+aprun python ./sampleSeq2Seq_data.py --indir ../../data/chat/json/rest1046 --indict ../../data/chat/txt/dict.dat --outfile ../../data/chat/txt/chat.dat --outdict ../../data/chat/txt/dict.dat
