88a13190d9ffe6144931ad0c28606d593b685bbd
k-utsubo <utsuboka@gmail.com>
2017-08-29 10:58:19 +0900

    multi

--- sampleCopySeq2Seq.sh
+++ sampleCopySeq2Seq.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #PBS -N copy_seq2seq
 #PBS -j oe 
-#PBS -l ncpus=1
+#PBS -l select=1:ncpus=36:ompthreads=36
 #PBS -q SINGLE
 
 if [ "${PBS_O_WORKDIR}" != "" ];then
   cd ${PBS_O_WORKDIR}
   export PYTHON_EGG_CACHE=/work/s1630401/tmp/.python-eggs
-  export OMP_NUM_THREADS=1
+  export OMP_NUM_THREADS=36
 fi
 source ~/.bashrc
 
-aprun python sampleCopySeq2Seq.py --dict ../../data/chat/model/dict.dat --data ../../data/chat/model/chat.dat --mode train --model ../../data/chat/model/copy/copy.model  --epoch 100
+aprun -n 1 -d $OMP_NUM_THREADS python sampleCopySeq2Seq.py --dict ../../data/chat/model/dict.dat --data ../../data/chat/model/chat.dat --mode train --model ../../data/chat/model/copy/copy.model  --epoch 100
 #aws s3 cp --recursive ../../data/chat/txt  s3://kyodonews.advanced/utsubo/nlp/chat
 #for f in *.txt;do
 #  aws s3 cp $f  s3://kyodonews.advanced/utsubo/nlp/chat/
