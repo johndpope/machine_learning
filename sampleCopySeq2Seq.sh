91c8eeaf2ebf86e02015cce6f9a221aa99f1ed8f
k-utsubo <utsuboka@gmail.com>
2017-09-06 17:54:05 +0900

    add

--- sampleCopySeq2Seq.sh
+++ sampleCopySeq2Seq.sh
@@ -11,7 +11,7 @@ if [ "${PBS_O_WORKDIR}" != "" ];then
 fi
 source ~/.bashrc
 
-aprun -n 1 -d $OMP_NUM_THREADS python sampleCopySeq2Seq.py --dict ../../data/chat/model/dict.dat --data ../../data/chat/model/chat.dat --mode train --model ../../data/chat/model/copy/copy.model  --epoch 100
+aprun -n 1 -d $OMP_NUM_THREADS python sampleCopySeq2Seq.py --dict ../../data/chat/model/dict.dat --data ../../data/chat/model/chat.dat --mode train --model ../../data/chat/model/copy/copy2.model  --epoch 300 --batch 50
 #aws s3 cp --recursive ../../data/chat/txt  s3://kyodonews.advanced/utsubo/nlp/chat
 #for f in *.txt;do
 #  aws s3 cp $f  s3://kyodonews.advanced/utsubo/nlp/chat/
